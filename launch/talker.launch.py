e6dbdfdc50f12f2f85faa0fed8e6a3a175cf837c
qunbui <quanbui.ius@gmail.com>
2024-12-11 16:03:07 +0700

    first commit

---
+++ launch/talker.launch.py
@@ -0,0 +1,10 @@
+import launch import LaunchDescription 
+import launch_ros.actions import Node 
+
+def generate_launch_description(): 
+    return LaunchDescription([ 
+        Node( 
+            package='demo_node_cpp', 
+            executable='talker' 
+        ) 
+    ]) 
